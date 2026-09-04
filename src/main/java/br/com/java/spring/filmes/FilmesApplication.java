cba95a66dd00db8f97b00620f18c7ebabf40e1fb
Douglas Soares <douglassoares.cinema@gmail.com>
2026-09-03 22:49:11 -0300

    feat: funcao de busca

--- src/main/java/br/com/java/spring/filmes/FilmesApplication.java
+++ src/main/java/br/com/java/spring/filmes/FilmesApplication.java
@@ -1,14 +1,21 @@
 package br.com.java.spring.filmes;
 
+import br.com.java.spring.filmes.controller.ConverteDados;
 import br.com.java.spring.filmes.model.Filme;
 import br.com.java.spring.filmes.service.FilmeService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import tools.jackson.databind.ObjectMapper;
+
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class FilmesApplication implements CommandLineRunner{
+
+	public static void exibeMenu(){
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FilmesApplication.class, args);
@@ -16,15 +23,41 @@ public class FilmesApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
+		Scanner scan = new Scanner(System.in);
+		ConverteDados mapper = new ConverteDados();
 		System.out.println("---------------------------------------------------------------");
-		ObjectMapper mapper = new ObjectMapper();
 
-		String filme = FilmeService.consultaFilme("tt0083658");
-		System.out.println(filme);
+		loop:
+		while(true){
+			exibeMenu();
+			int opcao = scan.nextInt();
 
-		var f = mapper.readValue(filme, Filme.class);
+			switch(opcao){
+				case 1:
+					/*
+					EXEMPLOS DE IDS VÁLIDOS:
+					tt0083658 (Blade Runner)
+					tt0276919 (Dogville)
+					tt7282468 (Burning)
+					tt6587046 (The Boy and The Heron)
+					tt1659337 (The Perks of Being a Wallflower)
+					 */
+					System.out.println("Digite o ID de um filme: ");
+					String id = scan.next();
 
-		System.out.println(f);
+					String filme = FilmeService.consultaFilme(id);
+					var f = mapper.obterDados(filme, Filme.class);
+
+					System.out.println(f);
+					break;
+				case 2:
+
+					break;
+				case 3:
+					scan.close();
+					break loop;
+			}
+		}
 
 	}
 }
--- src/main/java/br/com/java/spring/filmes/controller/ConverteDados.java
+++ src/main/java/br/com/java/spring/filmes/controller/ConverteDados.java
@@ -1,2 +1,12 @@
-package br.com.java.spring.filmes.controller;public class ConverteDados {
+package br.com.java.spring.filmes.controller;
+
+import br.com.java.spring.filmes.model.Filme;
+import tools.jackson.databind.ObjectMapper;
+
+public class ConverteDados{
+    private ObjectMapper mapper = new ObjectMapper();
+
+    public <T> T obterDados(String json, Class<T> classe) {
+        return mapper.readValue(json, (Class<T>) Filme.class);
+    }
 }
